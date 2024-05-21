64c22021f6eb78e389f6ae109ed2d75642df1c33
Fabrizio-padula <fabriziopadula80@gmail.com>
2024-05-21 16:38:03 +0200

    feat: adding movie table from database and printing on page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
-    //
+    public function index() {
+        $movies = Movie::all();
+
+        
+
+        $data = [
+            'movie' => $movies,
+        ];
+        
+        
+
+        return view('home', $data);
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Home</h1>
+    <div>
+        <h1>Film</h1>
+        @foreach ($movie as $singleMovie)
+                {{-- Single book template --}}
+                <div class="col">
+                    <div class="movie my-3">
+                        <h3>{{ $singleMovie->title }}</h3>
+                        <div>Autore: {{ $singleMovie->nationality }}</div>
+                        <div>Pagine: {{ $singleMovie->vote }}</div>
+                    </div>
+                </div>        
+            @endforeach
+        
+    </div>
 @endsection
